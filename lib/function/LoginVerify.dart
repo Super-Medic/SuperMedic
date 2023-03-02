7f822ef3303d321f9e0041acffbc1f94cd34b370
Byounguk An <dbzl159753@naver.com>
2023-03-03 04:00:05 +0900

    userSecession

--- lib/function/LoginVerify.dart
+++ lib/function/LoginVerify.dart
@@ -15,4 +15,16 @@ class Login_verify {
       return "false_err";
     }
   }
+
+  Future<String> userSecession(String email) async {
+    try {
+      final response = await http.post(
+        Uri.https('mypd.kr:5000', '/user/secession'),
+        body: {"account_email": email},
+      );
+      return response.body;
+    } catch (error) {
+      return "false_err";
+    }
+  }
 }
