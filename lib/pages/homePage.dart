b8618b5b1e78e6c716fca80014b7a1390b05d6da
hyun-stone <hyun698207@gmail.com>
2023-03-14 17:56:44 +0900

    03/14 merge and push

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -22,7 +22,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late HomeProvider _homeProvider;
   final storage = const FlutterSecureStorage();
-
   @override
   void initState() {
     super.initState();
--- lib/widgets/calender_widgets/utils.dart
+++ lib/widgets/calender_widgets/utils.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:super_medic/function/model.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:super_medic/widgets/calender_widgets/itemClass.dart';
 import 'package:super_medic/widgets/calender_widgets/calenderCheckBox.dart';
 import 'package:http/http.dart' as http;
 // import 'package:super_medic/widgets/calender_widgets/itemClass.dart';
@@ -29,15 +30,15 @@ final kEvents = LinkedHashMap<DateTime, List<Event>>(
   equals: isSameDay,
   hashCode: getHashCode,
 )..addAll(_kEventSource);
-
-final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
+// final _kEventSource = null;
+final _kEventSource = Map.fromIterable(List.generate(0, (index) => index),
     key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
     value: (item) => List.generate(
         item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
   ..addAll({
     kToday: [
-      Event('Today\'s Event 1'),
-      Event('Today\'s Event 2'),
+      // Event('Today\'s Event 1'),
+      // Event('Today\'s Event 2'),
     ],
   });
 
@@ -58,17 +59,41 @@ final kToday = DateTime.now();
 final kFirstDay = DateTime(kToday.year, kToday.month - 3, kToday.day);
 final kLastDay = DateTime(kToday.year, kToday.month, kToday.day);
 
-getData() async {
-  String? userEmail;
+Future<void> fetchPastGet() async {
   const storage = FlutterSecureStorage();
+  String? userEmail;
   String? val = await storage.read(key: 'LoginUser');
   if (val != null) {
     userEmail = LoginModel.fromJson(jsonDecode(val)).email;
   }
-  final res = await http
-      .get(Uri.parse('https://mypd.kr:5000/medicine/take?email=$userEmail'));
-  List<List> temp = List.empty(growable: true);
+  final res = await http.get(
+      Uri.parse('https://mypd.kr:5000/medicine/parse/past?email=$userEmail'));
+  List<Map> temp = List.empty(growable: true);
   if (res.body != 'Empty') {
-    // for (var data in json.decode(res.body)) {}
+    for (var data in json.decode(res.body)) {
+      // print(data['date']);
+      Map<String, List<Check>> checkMap = <String, List<Check>>{};
+      List<Check> checks = List.empty(growable: true);
+      data['takeinfo'].forEach((key, value) {
+        //     checks.add(Check(
+        // id: 0,
+        // medicine: key,
+        // time: info['time'],
+        // isChecked: info['check']));
+
+        print('key is $key');
+        print('value is $value ');
+      });
+      // for (var i = 0; i < data['takeinfo'].length; i++) {
+      // checks.add(Check(
+      //     id: data['id'],
+      //     medicine: data['medicine_name'],
+      //     time: info['time'],
+      //     isChecked: info['check']));
+      // }
+      // checkMap[data['date']] = data['takeinfo'];
+      // temp.add(checkMap);
+      // }
+    }
   }
 }
